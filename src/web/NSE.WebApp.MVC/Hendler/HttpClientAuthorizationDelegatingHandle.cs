67f900c7999001dce93ceea5a1b6fc2901e346c4
dell <dell@DESKTOP-P8QI2NJ>
2026-02-08 16:51:46 -0300

    Usando o Dellegate para o usúario conseguir seleciona o produto do catálogo

--- src/web/NSE.WebApp.MVC/Hendler/HttpClientAuthorizationDelegatingHandle.cs
+++ src/web/NSE.WebApp.MVC/Hendler/HttpClientAuthorizationDelegatingHandle.cs
@@ -1,6 +1,34 @@
-﻿namespace NSE.WebApp.MVC.Hendler
+﻿using NSE.WebApp.MVC.Extensions;
+using System.Net.Http.Headers;
+
+namespace NSE.WebApp.MVC.Hendler
 {
-    public class HttpClientAuthorizationDelegatingHandle
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
     {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            }
+
+            var token = _user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
     }
 }
